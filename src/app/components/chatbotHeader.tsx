ba36ff8f55908ab1d0338687ec2a932a8ecbc7f7
ASAD ULLAH KHALID <asad.khalid@mercedes-benz.com>
2024-05-06 16:49:15 +0200

    enhance: add localStorage to store chat

--- src/app/components/chatbotHeader.tsx
+++ src/app/components/chatbotHeader.tsx
@@ -10,17 +10,15 @@ export default function ChatbotHeader({
 }: ChatbotHeaderProps) {
   return (
     <button onClick={() => setShowChatbot(!showChatbot)}>
-      <div className={clsx("flex bg-c-dark border-2 text-white p-4 h-14")}>
-        <div className="flex flex-1 justify-center">
-          <p className="mr-2">AI Assistant</p>
-          <Image
-            alt="ai icon"
-            draggable="false"
-            src={IconMagic}
-            width="24"
-            height="24"
-          />
-        </div>
+      <div className="flex justify-center bg-c-dark border-2 text-white p-4 max-h-16">
+        <p className="mr-2">AI Assistant</p>
+        <Image
+          alt="ai icon"
+          draggable="false"
+          src={IconMagic}
+          width="24"
+          height="24"
+        />
       </div>
     </button>
   );
--- src/app/hooks/useChatbot.tsx
+++ src/app/hooks/useChatbot.tsx
@@ -1,10 +1,23 @@
-import { FormEvent, useState } from "react";
+import { FormEvent, useState, useEffect } from "react";
 import { getReplyFromChatbot } from "../utils/api";
 import { ChatbotMessage } from "../utils/types";
 
 export default function useChatbot() {
-  const [messages, setMessages] = useState<ChatbotMessage[]>([]);
   const [userInput, setUserInput] = useState("");
+  const [messages, setMessages] = useState<ChatbotMessage[]>([]);
+
+  // Load messages from local storage when the component mounts
+  useEffect(() => {
+    const savedMessages = localStorage.getItem("messages");
+    savedMessages &&
+      JSON.parse(savedMessages).length &&
+      setMessages(JSON.parse(savedMessages));
+  }, []);
+
+  // Save messages to local storage whenever they change
+  useEffect(() => {
+    localStorage.setItem("messages", JSON.stringify(messages));
+  }, [messages]);
 
   /**
    * Handles the form submission event when sending a message.
