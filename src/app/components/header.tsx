6db35f93d33ade4275c134ea5cf8c8c339cf582b
ASAD ULLAH KHALID <asad.khalid@mercedes-benz.com>
2024-05-07 21:10:12 +0200

    enhance: complete header links

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -5,6 +5,10 @@ import Image from "next/image";
 import LogoDark from "public/images/logo-dark.png";
 import LogoLight from "public/images/logo-light.png";
 import useHeader from "../hooks/useHeader";
+import Link from "next/link";
+import { header } from "../utils/constants";
+
+const { links } = header;
 
 export default function Header() {
   const { isDark } = useHeader();
@@ -27,9 +31,15 @@ export default function Header() {
           width="144"
           height="30"
         />
-        <ul className="hidden space-x-4 uppercase font-medium text-xl md:show md:flex">
-          <li>About</li>
-          <li>Contact</li>
+        <ul className="hidden space-x-4 uppercase font-medium text-lg md:show md:flex">
+          {links.map((link) => (
+            <li
+              key={link.href}
+              className="transition-colors duration-300 ease-in-out hover:text-blue-500"
+            >
+              <Link href={link.href}>{link.name}</Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
