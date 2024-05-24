fd667f537ceb41881a817b7eea77b552bdd35b27
ASAD ULLAH KHALID <asad.khalid@mercedes-benz.com>
2024-05-24 12:24:58 +0200

    enhance: add transition effect on all hover elements

--- src/app/about/components/general/tooltip.tsx
+++ src/app/about/components/general/tooltip.tsx
@@ -17,7 +17,7 @@ export default function Tooltip({ text, children }: TooltipProps) {
       className="relative"
     >
       {showTooltip && (
-        <div className="absolute w-96 mb-2 p-4 text-left bottom-full left-1/2 bg-c-dark text-c-light text-md text-normal rounded-md z-10transform -translate-x-1/2">
+        <div className="absolute w-96 mb-2 p-4 text-left bottom-full left-1/2 bg-c-dark text-c-light text-md text-normal rounded-md z-10transform -translate-x-1/2 transition-all duration-300 ease-in-out">
           {text}
         </div>
       )}
